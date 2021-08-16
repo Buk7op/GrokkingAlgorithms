53b00aada2e8107e6bd7d61926f4990003101661
Buk7op <robxviktor@gmail.com>
2021-08-16 22:55:54 +0300

    Added recursive Factorial

---
+++ Recursion/Factorial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(5));
+        }
+        static int Factorial(int i)
+        {
+            if(i == 1) return 1;
+            else return i * Factorial(i - 1);
+        }
+    }
+}
