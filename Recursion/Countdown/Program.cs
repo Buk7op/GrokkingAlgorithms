a0bbebbf44e08ac720a523d366a679d1adcbe2b7
Buk7op <robxviktor@gmail.com>
2021-08-16 22:44:16 +0300

    Added recursion countdown

---
+++ Recursion/Countdown/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Countdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Countdown(3);
+        }
+        static void Countdown(int i)
+        {
+            Console.WriteLine(i + "...");
+            if(i > 0)
+            Countdown(i - 1);
+        }
+    }
+}
