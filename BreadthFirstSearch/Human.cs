6b394bdf22ee33a6b71edaec66d1c97f3b01e6bb
Buk7op <robxviktor@gmail.com>
2021-08-19 21:08:32 +0300

    Added a BFSearch

---
+++ BreadthFirstSearch/Human.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+namespace BreadthFirstSearch
+{
+    class Human
+    {
+        public Human(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+        List<Human> HumanList = new List<Human>();
+        public List<Human> Friends 
+        {
+            get
+            {
+                return HumanList;
+            }
+        }
+        public void isFriendOf(Human h)
+        {
+            HumanList.Add(h);
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ BreadthFirstSearch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BreadthFirstSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human Valera = new Human("Valera");
+            Human Vitya = new Human("Vitya");
+            Human Nikita = new Human("Nikita");
+            Human Nastya = new Human("Nastya");
+            Human Maya = new Human("Maya");
+            Vitya.isFriendOf(Valera);
+            Vitya.isFriendOf(Nikita);
+            Nikita.isFriendOf(Nastya);
+            Valera.isFriendOf(Maya);
+            Console.WriteLine(BFSearch(Vitya,"Maya"));
+            
+        }
+
+        static string BFSearch(Human root, string searchingName)
+        {
+            Queue<Human> humans = new Queue<Human>();
+            HashSet<Human> searchedHumans = new HashSet<Human>();
+            humans.Enqueue(root);
+            searchedHumans.Add(root);
+            while (humans.Count > 0)
+            {
+                Human node = humans.Dequeue();
+                if(node.Name == searchingName)  return node.Name;
+                foreach(Human friend in node.Friends)
+                {
+                    if(!searchedHumans.Contains(friend))
+                    {
+                        humans.Enqueue(friend);
+                        searchedHumans.Add(friend);
+                    }
+                }
+            }
+            return $"Не найден человек с именем {searchingName}";
+        }
+    }
+}
